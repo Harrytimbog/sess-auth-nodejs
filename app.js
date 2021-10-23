dbc60278bce6d8cef68db8dbae58775979a2b596
Timilehin Arigbede <Harriemannie@gmail.com>
2021-10-23 12:43:42 +0100

    Session Authentication in Express

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const session = require('express-session')
 
 const TWO_HOURS = 1000 * 60 * 60 * 2
@@ -14,7 +15,18 @@ const {
 const IN_PROD = NODE_ENV === 'production'
 
 
+// TODO DB
+const users = [
+  { id: 1, name: 'Timi', email: 'timi@gmail.com', password: 'secret'},
+  { id: 1, name: 'Harrie', email: 'harrie@gmail.com', password: 'secret'},
+  { id: 1, name: 'Alex', email: 'alex@gmail.com', password: 'secret'}
+]
+
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(session({
   name: SESS_NAME,
@@ -28,6 +40,146 @@ app.use(session({
   }
 }))
 
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/login')
+  } else {
+    next()
+  }
+}
+
+const redirectHome = (req, res, next) => {
+  if (req.session.userId) {
+    res.redirect('/home')
+  } else {
+    next()
+  }
+}
+
+app.use((req, res, next) => {
+  const {userId } = req.session
+  if (userId) {
+    res.locals.user = users.find(
+      user => user.id === req.session.userId
+    )
+  }
+  next()
+})
+
+
+app.get('/', (req, res) => {
+  // console.log(req.session)
+  const { userId } = req.session;
+  res.send(`
+    <h1>Welcome!</h1>
+    ${userId ? `
+      <a href='/home'>Home</a>
+      <form method='post' action='/logout'>
+        <button>Logout</button>
+      </form>
+    ` : `
+      <a href='/login'>Login</a>
+      <a href='/register'>Register</a>
+    `}
+  `)
+})
+
+app.get('/home', redirectLogin, (req, res) => {
+  const { user } = res.locals
+
+  res.send(`
+    <h1>Home</h1>
+    <a href='/'>Main</a>
+    <ul>
+    <li>Name: ${user.name}</li>
+    <li>Email: ${user.email}</li>
+    </ul>
+  `)
+})
+
+// app.get('/profile', redirectLogin, (req, res) => {
+//   const { user } = res.locals
+// })
+
+app.get('/login', redirectHome, (req, res) => {
+  // req.session.userId
+  res.send(`
+    <h1>Login</h1>
+    <form method='post' action='/login'>
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Password' required />
+      <input type='submit' />
+    </form>
+    <a href='/register'>Register</a>
+  `)
+})
+
+app.get('/register', redirectHome, (req, res) => {
+  res.send(`
+    <h1>Register</h1>
+    <form method='post' action='/register'>
+      <input type='name' name='name' placeholder='Name' required />
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Password' required />
+      <input type='submit' />
+    </form>
+    <a href='/login'>Login</a>
+  `)
+})
+
+app.post('/login', redirectHome, (req, res) => {
+  const {email, password } = req.body
+
+  if (email && password) { // TODO validation
+    const user = users.find(
+      user => user.email === email && user.password === password
+    )
+
+    if (user) {
+      req.session.userId = user.id
+      return res.redirect('/home')
+    }
+  }
+
+  res.redirect('/login')
+})
+
+app.post('/register', redirectHome, (req, res) => {
+  const { name, email, password } = req.body
+
+  if (name && email && password) { // TODO validation
+    const exists = users.some(
+      user => user.email === email
+    )
+
+    if (!exists) {
+      const user = {
+        id: users.length + 1,
+        name,
+        email,
+        password // TODO hash
+      }
+
+      users.push(user)
+
+      req.session.userId = user.id
+      return res.redirect('/home')
+    }
+  }
+
+  res.redirect('/register') // TODO qs errors
+})
+
+app.post('/logout', redirectLogin, (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.redirect('/home')
+    }
+
+    res.clearCookie(SESS_NAME)
+    res.redirect('/login')
+  })
+})
 
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
 
